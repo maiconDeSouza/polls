b8e60b41389a443c7d30b2c85f3dd2e7b5a6de23
Maicon Souza <maicon_souza09@hotmail.com>
2025-03-05 21:37:56 -0300

    all that remains is to create new polls

--- setup/accounts/views.py
+++ setup/accounts/views.py
@@ -1,11 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
+
 
 class Login(View):
     def get(self, request):
         login_form = AuthenticationForm()
         context = {
-            'login_form' : login_form
+            'login_form': login_form
         }
         return render(request, 'accounts/pages/login.html', context)
+
+    def post(self, request):
+        login_form = AuthenticationForm(request, data=request.POST)
+
+        if login_form.is_valid():
+            user = login_form.get_user()
+            login(request, user)
+            return redirect('index')
+        else:
+            return redirect('login')
+
+
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect('index')
+
+
+class Register(View):
+    def get(self, request):
+        user_form = UserCreationForm()
+        context = {
+            'user_form': user_form
+        }
+        return render(request, 'accounts/pages/register.html', context)
+
+    def post(self, request):
+        user_form = UserCreationForm(request.POST)
+        context = {
+            'user_form': user_form
+        }
+
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('login')
+
+        return render(request, 'accounts/pages/register.html', context)
--- setup/polls/views.py
+++ setup/polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.utils.decorators import method_decorator
 from django.views import View
 
 
-from setup.polls.models import Poll, Choice
+from setup.polls.models import Poll, Choice, VotedPoll
 
 
 # Create your views here.
@@ -16,10 +18,16 @@ class Index(View):
         return render(request, 'polls/pages/index.html', context)
 
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class PollDetail(View):
     def get(self, request, pk):
         poll = get_object_or_404(Poll, pk=pk)
         choices = get_list_or_404(Choice, poll=pk)
+        voted = VotedPoll.objects.filter(user=request.user, poll=poll).exists()
+
+        if voted:
+            return redirect('poll_votes', pk=pk)
+
         context = {
             'poll': poll,
             'choices': choices
@@ -27,14 +35,18 @@ class PollDetail(View):
         return render(request, 'polls/pages/poll_detail.html', context)
 
     def post(self, request, pk):
+        poll = get_object_or_404(Poll, pk=pk)
         choice_id = request.POST.get('choice')
         choice = get_object_or_404(Choice, pk=choice_id)
         choice.votes += 1
         choice.save()
 
+        VotedPoll.objects.create(user=request.user, poll=poll)
+
         return redirect('poll_votes', pk=pk)
 
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class PollVotes(View):
     def get(self, request, pk):
         poll = get_object_or_404(Poll, pk=pk)
@@ -42,7 +54,8 @@ class PollVotes(View):
         total_votes = poll.total_votes()
 
         choices_with_percentage = [
-            {'choice_text': choice.choice_text, 'percentage': f'{((choice.votes / total_votes) * 100 if total_votes > 0 else 0):.2f}%'}
+            {'choice_text': choice.choice_text,
+                'percentage': f'{((choice.votes / total_votes) * 100 if total_votes > 0 else 0):.2f}%'}
             for choice in choices
         ]
 
@@ -51,3 +64,12 @@ class PollVotes(View):
             'choices': choices_with_percentage
         }
         return render(request, 'polls/pages/poll_votes.html', context)
+
+
+@method_decorator(user_passes_test(lambda user: user.is_staff, login_url='login'), name='dispatch')
+class NewPoll(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
